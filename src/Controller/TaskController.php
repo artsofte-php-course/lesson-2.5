65e915cba2a6f24b462a8337e2dee39f3c8ee742
leenwood <george200135@gmail.com>
2022-04-09 13:30:51 +0500

    lesson 2.5

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Task;
+use App\Type\TaskFilterType;
 use App\Type\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,7 +28,7 @@ class TaskController extends AbstractController
             $this->getDoctrine()->getManager()->persist($task);
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('task_success');
+            return $this->redirectToRoute('task_list');
         }
 
         return $this->render("task/create.html.twig", [
@@ -37,12 +38,83 @@ class TaskController extends AbstractController
     }
 
     /**
-     * @Route("/task/success", name="task_success")
+     * @Route("/tasks", name="task_list")
+     * @param $request
      * @return Response
      */
-    public function success(): Response
+    public function list(Request $request): Response
     {
 
-        return new Response();
+        $taskFilterForm = $this->createForm(TaskFilterType::class);
+
+        $taskFilterForm->handleRequest($request);
+
+        if ($taskFilterForm->isSubmitted() && $taskFilterForm->isValid())
+        {
+            $filter = $taskFilterForm->getData();
+
+            if($filter['isCompleted'] === null)
+            {
+                unset($filter['isCompleted']);
+            }
+
+            $tasks = $this->getDoctrine()
+                ->getRepository(Task::class)
+                ->findBy($filter, [
+                    'dueDate' => 'ASC',
+                ]);
+        }
+        else
+        {
+            $tasks = $this->getDoctrine()
+                ->getManager()
+                ->getRepository(Task::class)
+                ->findBy([], [
+                    'dueDate' => 'ASC'
+                ]);
+        }
+
+        return $this->render('task/list.html.twig', [
+            'tasks' => $tasks,
+            'filterForm' => $taskFilterForm->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/tasks/{id}/complete", name="task_complete")
+     * @return \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function complete($id)
+    {
+        $task = $this->getDoctrine()->getManager()->find(Task::class, $id);
+        if ($task === null)
+        {
+            return $this->createNotFoundException(sprintf("Not found id %s", $id));
+        }
+
+        $task->setIsCompleted(true);
+        $this->getDoctrine()->getManager()->persist($task);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute("task_list");
+    }
+
+    /**
+     * @Route("/tasks/{id}/undo", name="task_undo")
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $task = $this->getDoctrine()->getManager()->find(Task::class, $id);
+        if ($task === null)
+        {
+            return $this->createNotFoundException(sprintf("Not found id %s", $id));
+        }
+
+        $task->setIsCompleted(false);
+        $this->getDoctrine()->getManager()->persist($task);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute("task_list");
     }
 }
